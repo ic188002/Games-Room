c959dd6eeaa4479b2cb0b36707c6ec7175e59372
Ivan Craig <ic188002@falmouth.ac.uk>
2022-08-25 20:03:27 +0100

    1 more push

--- script.js/script.js
+++ script.js/script.js
@@ -1,23 +1,19 @@
 
-window.addEventListener('DOMContentLoaded', () => {
-    const tiles = Array.from(document.querySelectorAll('.tile'));
+    
+//setting out the var user selections
     const playerDisplay = document.querySelector('.display-player');
+    const tiles = Array.from(document.querySelectorAll('.tile'));
     const resetButton = document.querySelector('#reset');
     const announcer = document.querySelector('.announcer');
 
-
-
-    
-    let board = ['', '', '', '', '', '', '', '', ''];
-    let currentPlayer = 'X';
+// starting var
+    let currentUser = 'O';
     let isGameActive = true;
-
-    const PLAYERX_WON = 'PLAYERX_WON';
-    const PLAYERO_WON = 'PLAYERO_WON';
-    const TIE = 'TIE';
+    let Table = ['', '', '', '', '', '', '', '', ''];
 
 
-    //outline the winning arrays inside an array 
+
+//outline the winning arrays inside an array 
     const winningConditions = [
         [0, 1, 2],
         [3, 4, 5],
@@ -29,6 +25,13 @@ window.addEventListener('DOMContentLoaded', () => {
         [2, 4, 6]
     ];
 
+    // this adds an event listener to every tile in the array. when we click on the  tile the 
+    //user action functiuon will be called
+    tiles.forEach( (tile, index) => {
+        tile.addEventListener('click', () => userAction(tile, index));
+    });
+
+
     //function to control the back windown of the gun game
     let back = document.querySelector('.back');
     //select the .back class and asign it a varible
@@ -39,6 +42,10 @@ window.addEventListener('DOMContentLoaded', () => {
         back.style.left = 0;
         back.style.top = 500;
     });
+
+
+
+
     window.addEventListener('keyup', (e) => {
         switch (e.key) {
             case 'ArrowLeft':
@@ -47,79 +54,101 @@ window.addEventListener('DOMContentLoaded', () => {
             case 'ArrowRight':
                 back.style.left = parseInt(back.style.left) + howMuch + 'px';
                 break;
+               
           
           
         }
+        console.log(window)
     });
 
 
 
-    function handleResultValidation() {
+    function resultChecker() {
         let roundWon = false;
         for (let i = 0; i <= 7; i++) {
             const winCondition = winningConditions[i];
-            const a = board[winCondition[0]];
-            const b = board[winCondition[1]];
-            const c = board[winCondition[2]];
+            const a = Table[winCondition[0]];
+            const b = Table[winCondition[1]];
+            const c = Table[winCondition[2]];
             if (a === '' || b === '' || c === '') {
                 continue;
             }
             if (a === b && b === c) {
                 roundWon = true;
                 break;
+                
             }
         }
-
-
-
+    console.log(winningConditions[1])
  
+
+
+
     if (roundWon) {
-            announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
+            announce(currentUser === 'X' ? PLAYERX_WON : PLAYERO_WON);
             isGameActive = false;
             return;
         }
 
-    if (!board.includes(''))
+
+
+
+
+    if (!Table.includes(''))
         announce(TIE);
     }
 
 
 
     const updateBoard =  (index) => {
-        board[index] = currentPlayer;
+        Table[index] = currentUser;
     }
 
+
+
+
+
+
+//Change the player by removing and adding a class
     const changePlayer = () => {
-        playerDisplay.classList.remove(`player${currentPlayer}`);
-        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-        playerDisplay.innerText = currentPlayer;
-        playerDisplay.classList.add(`player${currentPlayer}`);
+        // removing the class list of the curently player
+        playerDisplay.classList.remove(`player${currentUser}`);
+        currentUser = currentUser === 'X' ? 'O' : 'X';
+        console.log(currentUser)
+        //X if it equals O and O if it equals X
+        playerDisplay.innerText = currentUser;
+        //update the player desiplay, with the class we have made in css
+        playerDisplay.classList.add(`player${currentUser}`);
     }
 
+
+
+
+
+
+
+
+//A Turn in the game
     const userAction = (tile, index) => {
         if(isValidAction(tile) && isGameActive) {
-            tile.innerText = currentPlayer;
-            tile.classList.add(`player${currentPlayer}`);
+             //check to see if its a valid function
+             // if it has an end game state or not
+            tile.innerText = currentUser;
+            //if both condition are turn we set the innertext to true (X, O)
+            tile.classList.add(`player${currentUser}`);
+            //asigning the new class based on current player
             updateBoard(index);
-            handleResultValidation();
+            //update the array
+            resultChecker();
+            //check if we have a winner or not 
             changePlayer();
+            //then change player methord
         }
     }
     
 
-    const announce = (type) => {
-        switch(type){
-            case PLAYERO_WON:
-                announcer.innerHTML = 'Player <span class="playerOO">O</span> Won';
-                break;
-            case PLAYERX_WON:
-                announcer.innerHTML = 'Player <span class="playerXX">X</span> Won';
-                break;
-            case TIE:
-                announcer.innerText = 'Tie';
-        }
-        announcer.classList.remove('hide');
-    };
+
+//checking that the move is possible
     const isValidAction = (tile) => {
         if (tile.innerText === 'X' || tile.innerText === 'O'){
             return false;
@@ -128,12 +157,16 @@ window.addEventListener('DOMContentLoaded', () => {
         return true;
     };
 
+
+
+
+
     const resetBoard = () => {
-        board = ['', '', '', '', '', '', '', '', ''];
+        Table = ['', '', '', '', '', '', '', '', ''];
         isGameActive = true;
         announcer.classList.add('hide');
 
-        if (currentPlayer === 'O') {
+        if (currentUser === 'O') {
             changePlayer();
         }
 
@@ -144,13 +177,37 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    tiles.forEach( (tile, index) => {
-        tile.addEventListener('click', () => userAction(tile, index));
-    });
 
 
-    resetButton.addEventListener('click', resetBoard);
-});
+    
+
+//Annouce the end game state to the user
+const announce = (type) => {
+    //recives an end game string called type, and based on that it will modifly the annoucment
+    switch(type){
+        case PLAYERO_WON:
+            announcer.innerHTML = 'Player <span class="playerOO">O</span> Won';
+            break;
+        case PLAYERX_WON:
+            announcer.innerHTML = 'Player <span class="playerXX">X</span> Won';
+            break;
+        case TIE:
+            announcer.innerText = 'Tie';
+    }
+    announcer.classList.remove('hide');
+    //we remove the hide class to show the annoument to the user 
+};
+
+//ending var
+    const PLAYERX_WON = 'PLAYERX_WON';
+    const PLAYERO_WON = 'PLAYERO_WON';
+    const TIE = 'TIE';
+
+
+
+
+ resetButton.addEventListener('click', resetBoard);
+
 
 
 
